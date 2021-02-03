5e3b6f97288b5533374179b7d01407f94c4856ae
Duy NGUYEN <duynguyen212@gmail.com>
2021-02-03 16:18:14 +0100

    A vuejs Adopt-Pet uses Vue Router and VueX

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Cats from '../views/Cats.vue'
+import Dogs from '../views/Dogs.vue'
+import Pet from '../views/Pet.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,23 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/cats',
+    name: 'Cats',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/Cats.vue')
+    component: Cats
+  },
+  {
+    path: '/dogs',
+    name: 'Dogs',
+    component: Dogs
+  },
+  {
+    path: '/pet/:species/:id',
+    name: 'Pet',
+    component: Pet
   }
 ]
 
